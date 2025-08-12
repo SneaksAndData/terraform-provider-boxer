8d81b4285112282f80931a026b7f1b58db3e95f7
Vitalii Savitskii <visa@ecco.com>
2025-08-12 11:58:02 +0200

    Part of https://github.com/SneaksAndData/boxer-core/issues/18
    Use the upgraded Boxer Issuer API in boxer validator

--- examples/data_sources/boxer_token/main.tf
+++ examples/data_sources/boxer_token/main.tf
@@ -8,9 +8,10 @@ terraform {
 
 provider "boxer" {
   issuer_host = "http://localhost:8888/"
+  validator_host = "http://localhost:8081/"
 }
 
-resource "boxer_cedar_schema" "example"  {
+resource "boxer_issuer_cedar_schema" "example"  {
   id = "example"
   data_json = <<EOT
   {
@@ -54,7 +55,7 @@ EOT
 }
 
 resource "boxer_principal" "example" {
-  schema_id = boxer_cedar_schema.example.id
+  schema_id = boxer_issuer_cedar_schema.example.id
   data_json = <<EOT
 {
     "uid": {
@@ -98,16 +99,19 @@ resource "boxer_external_identity" "example" {
   identity_provider = "provider"
   id                   = "test_user"
   principal = {
-    schema_id = boxer_cedar_schema.example.id
+    schema_id = boxer_issuer_cedar_schema.example.id
     principal_id = boxer_principal.example.id
   }
 }
 
 data boxer_token "example" {
+  depends_on = [
+    boxer_external_identity.example
+  ]
   identity_provider = boxer_identity_provider.example.name
   auth = {
     # For testing purposes, provide the bearer token value manually
-    bearer = ""
+    bearer = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2VG1pRzRfcTNuQS1hOUMycmhkOURoNEdzR1VFSU9SaUI1ZTJFUkJNbDNvIn0.eyJleHAiOjE3NTQ5OTE3MTIsImlhdCI6MTc1NDk5MTY1MiwianRpIjoib25ydHJvOjQzYzg1ZWI2LWIyMjMtNGM0NS1iZDEyLTI4YTUwNmNjNDFiNyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiYWNjb3VudCIsInR5cCI6IkJlYXJlciIsImF6cCI6InRlc3RfY2xpZW50Iiwic2lkIjoiY2ZmMjUyZTItNWJmZS00ZDllLTg3Y2UtMTgzM2QwMjYzMGRlIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoidGVzdF91c2VyIn0.RiXVFdz7LHd0-bDd7_yM4gRrr5MHr37ysX0oEr5Eg6vVsioEIFn809fprd7XAodP_jBpjglcD12bhS3JCPxAIx_GQYj0Iqbkunj1PcFL_xkfflQmcc0dRf1izpol6X-K61FvnTtvG5TP_EJspKR8OeOuQCl788ES-vf4WooYuE54wRU4NngqChG7Q7KOrsbNlAjxZMGjaOv9C8fo7oNWNYGH9BSvYrabx06SIJjQjOICXIitO81um70HW3RGA61qMguCF5gkR94_xSrME_dsY1-EWvFQ65I-OTKnLSuh8i_ZwKqpd871m-zyAiFwXuna136nt4fNitEhMTXPpLcIkw"
   }
 }
 
