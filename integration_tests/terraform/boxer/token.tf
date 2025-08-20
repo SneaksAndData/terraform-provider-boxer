44337862b995008116d5ae9943141db9eba1e617
Vitalii Savitskii <visa@ecco.com>
2025-08-20 17:16:24 +0200

    Added necessary files for integration tests

--- integration_tests/terraform/boxer/token.tf
+++ integration_tests/terraform/boxer/token.tf
@@ -2,6 +2,6 @@ data boxer_token "example" {
   identity_provider = boxer_external_identity.alice.identity_provider
   auth = {
     # For testing purposes, provide the bearer token value manually
-    bearer = ""
+    bearer = var.external_token
   }
 }
