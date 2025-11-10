4b018b48b435fe8aa46faf4efcb4dc3f416abc3f
Vitalii Savitskii <visa@ecco.com>
2025-11-10 16:12:55 +0100

    Add policy

---
+++ internal/tests/validator_tests/data_source_policy_set_test.go
@@ -0,0 +1,40 @@
+package issuer_tests
+
+import (
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"terraform-provider-boxer/internal/tests"
+	"terraform-provider-boxer/internal/tests/assertions"
+	"testing"
+)
+
+func TestDataSourceCedarPolicySet_reading(t *testing.T) {
+
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	services := tests.NewLocalServices()
+	token, err := tests.GetExternalToken(services)
+	testContext := tests.NewTestContext(randomName, services, token)
+	if err != nil {
+		t.Fatalf("failed to get external token: %s", err)
+	}
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: tests.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: tests.RenderTemplate(testContext, "data_source_policy_set.tmpl.tf"),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.boxer_policy_set.example",
+						tfjsonpath.New("id"),
+						knownvalue.StringExact(randomName),
+					),
+					assertions.ValidatePolicySetCedarIsParseable("data.boxer_policy_set.example"),
+					assertions.ValidatePolicySetJsonIsParseable("data.boxer_policy_set.example"),
+				},
+			},
+		},
+	})
+}
